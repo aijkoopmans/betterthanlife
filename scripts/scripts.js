709a58218fbd2659a488f4e619a1d8782aa0f9fa
Arend <arend3571@hotmail.com>
2020-04-09 12:06:05 +0200

    mute unmute

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,12 +11,50 @@ $(document).ready(function(){
       }
   ]
   });
+
+  $('video').each(function () {
+      toggleMuteUnmuteButtons(`#${this.id}`);
+  });
+
+  $('.slick-scroll').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+    var video = document.querySelector(`#video${currentSlide+1}`);
+    var promise = video.pause();
+    var video = document.querySelector(`#video${nextSlide+1}`);
+    var promise = video.play();
+  });
 });
 
-// On before slide change
-$('.slick-scroll').on('beforeChange', function(event, slick, currentSlide, nextSlide){
-  var video = document.querySelector(`#video${currentSlide+1}`);
-  var promise = video.pause();
-  var video = document.querySelector(`#video${nextSlide+1}`);
+
+
+function muteUnmute(selector){
+  var video = document.querySelector(selector);
+  video.muted = !video.muted;
+  toggleMuteUnmuteButtons(selector);
+}
+
+function toggleMuteUnmuteButtons(selector){
+  var video = document.querySelector(selector);
+  var button = $(selector).parent().find(".mute-unmute");
+  if (video.muted) {
+    button.find(".mute").hide();
+    button.find(".unmute").show();
+  } else {
+    button.find(".mute").show();
+    button.find(".unmute").hide();
+  }
+}
+
+function ensurePlayFirstVideo(){
+  var video = document.querySelector('#video1');
   var promise = video.play();
-});
+
+  if (promise !== undefined) {
+    promise.then(_ => {
+      // Autoplay started!
+
+    }).catch(error => {
+      // Show something in the UI that the video is muted
+      video.muted = true;
+    });
+  }
+}
